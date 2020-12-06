0d6625db0317800d01e44d7492049ee586549699
Micah Levine <micah.levine@outlook.com>
2020-12-05 20:48:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() *11) + 2;
+const mod = 36/9;
+const max = Math.max(1, 3, 5, 10, 20);
